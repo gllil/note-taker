73124df870290861b1f4a980cbf4779a20231258
Gary Llil <garyjllil@gmail.com>
2020-03-24 10:24:16 -0400

    fixed the path by adding require path variable

--- develop/server.js
+++ develop/server.js
@@ -1,20 +1,25 @@
 const fs = require("fs");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "public/index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
-app.get("/api/notes", (res, req) => {
-    return res.json(notes)
-})
+
+
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+  });
